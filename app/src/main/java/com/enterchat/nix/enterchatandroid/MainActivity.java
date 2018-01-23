6f719bf5ed8acd6eacbc8fa9338d8eba953be8c6
Nikita K <novembernix@gmail.com>
2018-01-23 23:20:09 +0300

    Implementing Basic App Structure and Navigation Drawer Mock-up

--- app/src/main/java/com/enterchat/nix/enterchatandroid/MainActivity.java
+++ app/src/main/java/com/enterchat/nix/enterchatandroid/MainActivity.java
@@ -1,13 +1,43 @@
 package com.enterchat.nix.enterchatandroid;
 
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private FragmentManager mFragmentManager;
+    private Fragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mFragmentManager = getSupportFragmentManager();
+        mFragment = new SampleFragment();
+        mFragmentManager.beginTransaction().add(R.id.main_view, mFragment).commit();
+
+        NavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setCheckedItem(R.id.common_chat_item);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull final MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //выбор пунктов меню через switch
+                    }
+                }, 250);
+                return true;
+            }
+        });
     }
 }
